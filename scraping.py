890c637d38389c544729d9bc8b0e9244683008f2
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-09 18:25:22 -0500

    f(x) for scrape all

--- scraping.py
+++ scraping.py
@@ -2,12 +2,28 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
+import datetime as dt
 
 executable_path = {'executable_path': 'chromedriver.exe'}
 browser = Browser('chrome', **executable_path, headless=False)
 
+def scrape_all():
+    # Initiate headless driver for deployment
+    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+    news_title, news_paragraph = mars_news(browser)
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+}
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
-def mars_news(browser):
+def mars_news(browser): # scrape Mars news
     # Visit the mars nasa news site
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
@@ -16,10 +32,11 @@ def mars_news(browser):
 
     html = browser.html
     news_soup = soup(html, 'html.parser')
+
     # Add try/except for error handling
     try:
         slide_elem = news_soup.select_one('ul.item_list li.slide')
-        slide_elem.find("div", class_='content_title')
+        # slide_elem.find("div", class_='content_title')
         # Use the parent element to find the first `a` tag and save it as `news_title`
         news_title = slide_elem.find("div", class_='content_title').get_text()
         # Use the parent element to find the paragraph text
@@ -68,7 +85,8 @@ def mars_facts():
     # assigning columns and setting index
     df.columns=['description', 'value']
     df.set_index('description', inplace=True)
-
     return df.to_html()
 
-browser.quit()
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
