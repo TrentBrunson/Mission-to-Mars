e8a9efc261dd8963240481a880660e11fb6925fe
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-09 17:29:10 -0500

    created function to scrape Mars news

--- scraping.py
+++ scraping.py
@@ -10,35 +10,27 @@ executable_path = {'executable_path': 'chromedriver.exe'}
 browser = Browser('chrome', **executable_path, headless=False)
 
 
-# %%
-# Visit the mars nasa news site
-url = 'https://mars.nasa.gov/news/'
-browser.visit(url)
-# Optional delay for loading the page
-browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+def mars_news(browser):
+    # Visit the mars nasa news site
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    # Optional delay for loading the page
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+    # Add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
+        slide_elem.find("div", class_='content_title')
+        # Use the parent element to find the first `a` tag and save it as `news_title`
+        news_title = slide_elem.find("div", class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+    except AttributeError:
+        return None, None
 
-# %%
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('ul.item_list li.slide')
-
-
-# %%
-
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find("div", class_='content_title').get_text()
-news_title
-
-
-# %%
-
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
-news_p
-
-# %% [markdown]
-# ### Fetured Images
+    return news_title, news_p
 
 # %%
 # Visit URL
@@ -66,20 +58,16 @@ img_soup = soup(html, 'html.parser')
 img_url_rel = img_soup.select_one('figure.lede a img').get("src")
 img_url_rel
 
-
-# %%
 # Use the base URL to create an absolute URL
 img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
 img_url
 
 
-# %%
 # Mars facts
 df = pd.read_html('http://space-facts.com/mars/')[0]
 df.columns=['description', 'value']
 df.set_index('description', inplace=True)
-df
-# %%
+
 df.to_html()
-# %%
+
 browser.quit()
